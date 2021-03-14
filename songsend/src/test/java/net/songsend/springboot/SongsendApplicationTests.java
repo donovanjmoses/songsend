6ebe5f84db82c670735636302462c69557c5b665
donovanjmoses <donovanjmoses@macbook-pro.lan>
2021-03-14 12:15:37 -0500

    Initial Commit

---
+++ songsend/src/test/java/net/songsend/springboot/SongsendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.songsend.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
